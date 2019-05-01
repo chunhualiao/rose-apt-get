574d3ed26cd11606f42ba2005d7d8ec2af9476a1
gleisonsdm <gleison14051994@gmail.com>
2019-05-01 09:00:05 -0700

    Scripts updated to ubuntu 18.

--- build.sh
+++ build.sh
@@ -12,16 +12,16 @@ ROOT=$(pwd)
 
 #-------------------- install dependent software
 sudo apt update
-sudo apt install -y make gcc g++ gfortran gcc-5 g++-5 gfortran-5 libxml2-dev texlive git automake autoconf libtool flex bison openjdk-8-jdk debhelper devscripts ghostscript libboost-{chrono,date-time,filesystem,iostreams,program-options,random,regex,serialization,signals,system,thread,wave}-dev
+sudo apt install -y make gfortran gcc-7 g++-7 gfortran-7 libxml2-dev texlive git automake autoconf libtool flex bison openjdk-8-jdk debhelper devscripts ghostscript python lsb-core perl-doc libboost-{chrono,date-time,filesystem,iostreams,program-options,random,regex,serialization,signals,system,thread,wave}-dev
 
 #-------------------- clone and configure ROSE
-if [ ! -d "rose-develop" ]; then
-	git clone https://github.com/rose-compiler/rose-develop
-	(cd rose-develop && git am < ../0001-fix-rosePublicConfig.h-DESTDIR.patch)
+if [ ! -d "rose" ]; then
+	git clone https://github.com/rose-compiler/rose
+	(cd rose && git am < ../0001-fix-rosePublicConfig.h-DESTDIR.patch)
 fi
 
-if [ ! -f "rose-develop/configure" ]; then
-	(cd rose-develop && ./build)
+if [ ! -f "rose/configure" ]; then
+	(cd rose && ./build)
 fi
 
 # support reentry of this script
@@ -31,21 +31,22 @@ fi
 
 # note that --prefix is set to be /usr/rose
 # so this script must use sudo priviledge to run!!
-(cd rose-build && CC=gcc-5 CXX=g++-5 CXXFLAGS= ../rose-develop/configure --prefix=/usr/rose --with-C_OPTIMIZE=-O0 --with-CXX_OPTIMIZE=-O0 --with-C_DEBUG='-g' --with-CXX_DEBUG='-g' --with-boost=/usr --with-boost-libdir=/usr/lib/x86_64-linux-gnu/ --with-gfortran=/usr/bin/gfortran-5 --enable-languages=c,c++,fortran --enable-projects-directory --enable-edg_version=4.9)
+(cd rose-build && CC=gcc-7 CXX=g++-7 CXXFLAGS= ../rose/configure --prefix=/usr/rose --with-boost=/usr --with-boost-libdir=/usr/lib/x86_64-linux-gnu/ --enable-languages=c,c++ --without-java --enable-edg_version=5.0 --disable-boost-version-check --disable-tests-directory)
 if [ "$?" != "0" ]; then exit 1; fi
 
 #-------------------- build ROSE
-# -j$(nproc) may cause memory consumption issue on a virtual machine with limited memory. We use 1 process to be safe
+# -j$(nproc) may cause memory consumption issue on a virtual machine with limited memory. We use 2 process to be safe
 #(cd rose-build && make core -j$(nproc) && make DESTDIR=$ROOT/rose-install install-core -j$(nproc))
-(cd rose-build && make core && make DESTDIR=$ROOT/rose-install install-core)
+(cd rose-build && make core -j2 && make install-core -j2)
 if [ "$?" != "0" ]; then exit 1; fi
 
-ROSE_VERSION=$(cat rose-develop/ROSE_VERSION)
+sed -i '1s/^/#define __builtin_bswap16 __bswap_constant_16\n/' /usr/rose/include/edg/g++-7_HEADERS/hdrs7/bits/byteswap.h
+
+ROSE_VERSION=$(cat rose/ROSE_VERSION)
 ROSE_DEBIAN_BINARY_ROOT=rose-$ROSE_VERSION
 
 # support reentry of this script
 if [ ! -d "$ROSE_DEBIAN_BINARY_ROOT" ]; then
-#  rm -rf $ROSE_DEBIAN_BINARY_ROOT
   mkdir $ROSE_DEBIAN_BINARY_ROOT
 fi
 
@@ -64,9 +65,8 @@ done
 mkdir -p $ROSE_DEBIAN_BINARY_ROOT/etc/ld.so.conf.d
 cp 10-rose.conf $ROSE_DEBIAN_BINARY_ROOT/etc/ld.so.conf.d
 
-
 #================== make the debian package in a work directory inside the binary release candidate dir.
-ROSE_VERSION=$(cat rose-develop/ROSE_VERSION)
+ROSE_VERSION=$(cat rose/ROSE_VERSION)
 ROSE_DEBIAN_BINARY_ROOT=rose-$ROSE_VERSION
 
 if [ ! -d "$ROSE_DEBIAN_BINARY_ROOT/debian" ]; then
@@ -79,16 +79,16 @@ sed -i -e "s/\$VERSION/$ROSE_VERSION/g" $ROSE_DEBIAN_BINARY_ROOT/debian/changelo
 
 
 # remove possible stale package
-rm -rf rose_$(cat rose-develop/ROSE_VERSION)-2.orig.tar.gz
+rm -rf rose_$(cat rose/ROSE_VERSION)-2.orig.tar.gz
 
-tar cfz rose_$(cat rose-develop/ROSE_VERSION)-2.orig.tar.gz $ROSE_DEBIAN_BINARY_ROOT
+tar cfz rose_$(cat rose/ROSE_VERSION)-2.orig.tar.gz $ROSE_DEBIAN_BINARY_ROOT
 
 #--------sign your binary 
 ROOT=`pwd`
 echo "current path is $ROOT"
 
 if [ ! -f "$ROOT/pubkey" ]; then
-        keyVal=$(gpg --list-keys | awk '/sub/{if (length($2) > 0) print $2}')
+        keyVal=$(gpg --list-keys | sed -n 4p | tail -c 9)
         echo "${keyVal##*/}" > $ROOT/pubkey
 fi
 
@@ -100,7 +100,7 @@ if [ "x$SIGN_KEY" == "x" ]; then
 #somehow pubkey may be emtpy!
 # we regenerate it again
   rm -rf $ROOT/pubkey
-  keyVal=$(gpg --list-keys | awk '/sub/{if (length($2) > 0) print $2}')
+  keyVal=$(gpg --list-keys | sed -n 4p | tail -c 9)
   echo "${keyVal##*/}" > $ROOT/pubkey
 
   if [ "x$SIGN_KEY" == "x" ]; then
