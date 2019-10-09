1a1fe18a4b2c901eab287d2c84c6b966ac9eea46
Nathan Pinnow <pinnow2@llnl.gov>
2019-10-09 23:44:32 +0000

    Revert "Added serperation of tools"
    This reverts commit 8175df812ddb1fe0f3f1913348f7688d75cde516.

--- DockerBuilds/MakeAptGet/build-ROSE.sh
+++ DockerBuilds/MakeAptGet/build-ROSE.sh
@@ -5,6 +5,8 @@ cd
 git clone -b pinnow2 https://github.com/chunhualiao/rose-apt-get.git
 cd rose-apt-get
 ./build.sh
-
-#dput ppa:rosecompiler/rose-development rose_${ROSE_VERSION}-0_source.changes
-#dput ppa:rosecompiler/rose-development rose-tools_${ROSE_VERSION}-0_source.changes
+ROSE_VERSION=$(cat rose/ROSE_VERSION)
+dput ppa:rosecompiler/rose-development rose_${ROSE_VERSION}-0_source.changes
+if [[ $ROSE_VERSION == *.0 ]] ; then 
+    dput ppa:rosecompiler/rose rose_${ROSE_VERSION}-0_source.changes
+fi
